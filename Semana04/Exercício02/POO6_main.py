d546e8703a1feb8083b5151bcf6d9fa1e3e7d1c3
Rafael de Lima Gomes <rafaelimag2@gmail.com>
2024-02-04 11:03:55 -0300

    new files

---
+++ Semana04/Exercício02/POO6_main.py
@@ -0,0 +1,11 @@
+# Getters and Setters
+
+from POO6_item import Item
+
+item1 = Item("MyItem", 750)
+
+# Setting an Attribute
+item1.name = "OtherItem"
+
+# Getting an Attribute
+print(item1.name)
---
+++ Semana04/Exercício02/POO7_main.py
@@ -0,0 +1,10 @@
+# Getters and Setters
+
+from POO7_phone import Phone
+from POO7_keyboard import Keyboard
+
+item1 = Keyboard("jscKeyboard", 1000, 3)
+
+item1.apply_discount()
+
+print(item1.price)
