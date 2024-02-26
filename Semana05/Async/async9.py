d6fe7514f2fc838640d1439c6de1cf3549ffc4f8
Rafael de Lima Gomes] <rafaelimag2@gmail.com>
2024-02-25 23:53:16 -0300

    new files

--- Semana05/Async/async9.py
+++ Semana05/Async/async9.py
@@ -12,11 +12,9 @@ async def print_numbers():
         await asyncio.sleep(0.25)    
     
 async def main():
-    task1 = asyncio.create_task(fetch_data())
     task2 = asyncio.create_task(print_numbers())
+    await fetch_data()
     
-    value = await task1
-    print(value)
     await task2
     
 asyncio.run(main())
