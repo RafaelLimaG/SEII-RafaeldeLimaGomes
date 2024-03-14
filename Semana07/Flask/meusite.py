e7546fd72716f6a90bd8de7edb1f1052818a1247
Rafael de Lima Gomes <rafaelimag2@gmail.com>
2024-03-14 00:48:58 -0300

    new files

--- Semana07/Flask/meusite.py
+++ Semana07/Flask/meusite.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from random import randint
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
+def index():
+    variavel = "Game: Adivinhe o número correto"
+    
+    if request.method == "GET":
+        return render_template("index.html", variavel = variavel)
+    else:
+        numero = randint(1,5) 
+        palpite = int(request.form.get("name"))
+        if numero == palpite:
+            return '<h1>Resultado: Você Ganhou!</h1>'
+        else:
+            return '<h1>Resultado: Você Perdeu!</h1>'
 
-def ola_mundo():
-    return 'Olá Mundo'
+@app.route('/<string:nome>')
+def error(nome):
+    variavel = f'Página {nome} não encontrada'
+    return render_template("error.html", variavel = variavel)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
